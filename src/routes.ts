bb72fb43b75ccd14d7301b679a01f96f0183af7f
bonetou <h.boneto@outlook.com>
2023-05-11 16:01:52 -0300

    feat: adding image handler

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,17 @@
-import { Dataset, createCheerioRouter, extractUrls } from 'crawlee';
-
+import { Dataset, createCheerioRouter } from 'crawlee';
 export const router = createCheerioRouter();
+import fs from 'fs';
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-    log.info(`enqueueing new URLs`);
+router.addDefaultHandler(async ({ enqueueLinks, log,  }) => {
     await enqueueLinks({
         selector: '#chapters > div > a',
-        label: 'chapter-handler',
+        label: 'CHAPTER-HANDLER',
     });
 });
 
 
-router.addHandler('chapter-handler', async ({ request, $, log, enqueueLinks }) => {
-    const chapterTitle = $('#top').text();
+router.addHandler('CHAPTER-HANDLER', async ({ $, log, request, sendRequest }) => {
+    const chapterTitle = request.url.split('/').pop() || '';
     const chapterImages = $('img.js-page');
     const imageSources = chapterImages.map((i, chapterImage) => {
         return $(chapterImage).attr('data-src');
@@ -24,4 +23,21 @@ router.addHandler('chapter-handler', async ({ request, $, log, enqueueLinks }) =
         title: chapterTitle,
         images: imageSources,
     })
+    imageSources.forEach(async (imageSource) => {
+        let response = await sendRequest({
+            url: imageSource,
+            responseType: 'buffer',
+        })
+        const fileName = imageSource.split('/').pop()
+        log.info(`Saving chapter: ${chapterTitle} - image: ${imageSource}, fileName: ${fileName}`)
+        saveImage(response.rawBody, fileName, chapterTitle)
+    });
 });
+
+
+let saveImage = async (imageBuffer: Buffer, fileName: string, chapterTitle: string) => {
+    fs.mkdirSync(`./downloaded/${chapterTitle}`, { recursive: true });  
+    const writer = fs.createWriteStream(`./downloaded/${chapterTitle}/${fileName}`);
+    writer.write(imageBuffer);
+    writer.end();
+}
